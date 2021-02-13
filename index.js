424129b3fbd6042d4fa03b822de8b287207e93ad
shweta2kb <shweta2kb@gmail.com>
2021-02-13 05:46:54 +0000

    alignment work

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let dna = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 console.log(dna);
 dna = dna.replace('GCT','AGG');
 console.log(dna);
-
 //b) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
 if(dna.indexOf("CAT")){
   console.log("CAT gene found");
@@ -16,6 +15,5 @@ console.log(dna.slice(16,19));
 //d) Use a template literal to print, "The DNA strand is ___ characters long."
 let strcount=" testing string function";
 console.log("The DNA strand is "+ dna.length+" characters long");
-
 //e) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
 console.log(`${dna.slice(4,7).toLowerCase()}o ${dna.slice(dna.indexOf('CAT'),dna.indexOf('CAT')+3).toLowerCase()}`);
